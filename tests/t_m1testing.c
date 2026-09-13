b4e7c7a3bdf2134f2cdffeea5beda681b542e908
taiyouxp <thayo.ffs@gmail.com>
2026-09-12 23:25:31 -0300

    2nd commit: testing bytes offsets inside heap and basic I/O operations

--- tests/t_m1testing.c
+++ tests/t_m1testing.c
@@ -1,15 +1,13 @@
-/* 
-For now we will build: 
-a 4kb page, a 16 bytes header and a 8 bytes register
-    1. write_pg(n, bytes) and read_pg(n) in a common file. 
-	2. record a register inside slot 0 in the page 2, end the process, open it and then read the same register.
-	3. commit in the duo's repo with a NOTES.md, telling which byte of the file the register has gone. 
-*/
-
 #include <stdlib.h>
-#include <assert.h>
 #include <stdio.h>
-#define bytes 4096 // 4096 bytes - converted with char type
+#define bytes 4096 
+
+/* 
+testing language structures/features according to the proposal of M1:
+t1. persistence in a file 
+t2. page addressing
+t3. slot offset math
+*/
 
 struct pg 
 {
@@ -18,12 +16,12 @@ struct pg
     char r_data[bytes - 16]; 
     // slot 0: byte 0 to 7 -> there goes register 0 data
     // 4080 / 8 = 509 registers can be written
-} pg0, pg1; 
+} pg0, pg1, pg2; 
     
   
 void write_pg(int n); // n = page number. idk if we need the other parameter as i defined a macro. 
 
-/*    
+    /*    
     if (n == 0) 
         should init metadata 'page' telling how much pages are stored inside the file  
         (it should fetch the count of pages and its remaining size 
@@ -37,37 +35,76 @@ void read_pg(int n);
 
 int main(void)
 {
+    puts("[T1]\n"); 
+    // t1.1: seeing the memory allocation of the struct fields (should be contiguous - pass)
+    printf("page 0 address: %p\nheader address: %p\nregisters addresses (slots): %p\n", &pg0, &pg0.header, &pg0.r_data);
+    
+    // t1.2: seeing if that the declared struct allocated size is 4096 (pass)
+    printf("size of page: %zu bytes\n", sizeof(pg0)); 
 
-    // t1: seeing the memory allocation of the struct fields (should be contiguous - pass)
-/*     printf("%p %p %p \n", &pg_0, &pg_0.header, &pg_0.r_data);
-    
-    // t2: seeing if that the declared struct allocated is 4096 (pass)
-    printf("%zu\n", sizeof(pg_0)); */
-    
-    // t3: filling slot 0 with an array of 8 chars
-    // these kind of operation needs to be modularized later
-    char name[8] = "borabill"; int slot = 0; 
+    // t1.3: filling slot 0 with an array of 8 chars
+    // (these kind of operation needs to be modularized later)
+    char name[8] = "borabill"; 
     for (int i = 0; i < 8; ++i) 
     {
         pg0.r_data[i] = name[i]; 
-        if (i % 8 == 0) 
-        {
-            pg0.header[slot] += 1; // first slot, useless for now
-            slot++;     
-        } // wip :)
-    }
-    // t4: opening a file and using fwrite() to store the actual page 
-    FILE* f1 = fopen("file.bin", "wb");
-    assert(f1);
-    size_t r1 = fwrite(pg0.r_data, sizeof(pg0.r_data[0]), bytes, f1);
-    printf("wrote %zu elements out of %d requested\n", r1, bytes);
+    } 
 
+    puts(pg0.r_data);
     
-    FILE* f2 = fopen("file.bin", "rb");
-    size_t r2 = fread(pg1.r_data, sizeof pg1.r_data[0], bytes, f2);
-    // wip :) 
+    // t1.4 - t1.6: opening a file and using fwrite() to store
+    // the first 'slot' of page 0 then assign the same pattern for the 'page 1'
+    // to see the persistence of data. (pass)
+    FILE* f1 = fopen("file.bin", "wb");
+    fwrite(pg0.r_data, sizeof(pg0.r_data[0]), 8, f1);
+    fclose(f1);
+    
+    pg1 = pg0; 
+    
+    f1 = fopen("file.bin", "rb");
+    size_t r1 = fread(pg1.r_data, sizeof pg1.r_data[0], 8, f1);
+    if (r1 == 8)
+    {
+        printf("Array (register) at %p (slot 0 of page 1) read successfully, contents:\n", (void*)&pg1.r_data);
+        puts(pg1.r_data);
+    } 
+    fclose(f1);
+    
+    puts("\n[T2]\n");
+    /*
+     * t2.1 now calculating offsets
+        i want to make sure that the page 2 will be written in the correct address: 
+        - pg0: 0 -> 4096 
+        - pg1: 4096 -> 8192
+        - pg2: (2 * 4096) = 8192 -> ...
+    */
+    pg1 = pg2;
+
+    struct pg* ptr0 = &pg0; 
+    struct pg* ptr1 = &pg1;
+    struct pg* ptr2 = &pg2;
+    
+    long offset_st0, offset_st1, offset_st2; 
+    offset_st0 = (long) ptr0;
+    offset_st1 = (long) ptr1;
+    offset_st2 = (long) ptr2;
+    long offset2 = offset_st2 - offset_st0; // 2 * 4096
+
+    printf("%ld\n%ld\n%ld\n", offset_st0, offset_st2, offset2);
     return 1;
 }
-/* to do:
-    1. write the next steps properly   
-*/
+
+/* (12.11)
+ * So i've realized that this offset calculation to write a page into the actual right place inside the file is little bit more complex
+ * than i expected. atleast from these previously 'tests' i can infer the actual memory offset positions of the 'pages'  
+ * inside the program 'heap' (i think this is correct) which for my current goal is slot 0 for page 2 (8192).
+ * for tomorrow: 
+ *  1. resolve how to actually do the page addressing for the file and so fetch the data from there.
+ *  2. improve the way to identify actual registers instead of using a multiple of 8 as reference... 
+ *  3. finally define the structure of metadata page and the actual page and so making use of the header of each.
+ *  4. to finish M1: define the structure of write_pg(), read_pg(), the main loop to keep the program alive 
+ *  as a process so we can use kill command from the kernel (?).
+ *  
+ *  [bonus]. write the byte addresses automatically to NOTES.md with a 
+ *  helper function that could work as 'operations' log. 
+ * */
