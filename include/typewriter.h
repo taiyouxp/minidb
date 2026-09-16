782735c9feb078832907bfb371966cee1a679d5d
jean souza <741jean@gmail.com>
2026-09-16 20:24:59 -0300

    header file fixed

--- include/typewriter.h
+++ include/typewriter.h
@@ -1,28 +1,6 @@
 #ifndef TYPEWRITER_H
 #define TYPEWRITER_H
 
-#include <stdio.h>
-#include <time.h>
-#define HISTORY_PATH "../HISTORY.txt"
-// later today i will implement a way to show the author of the message before it
-
-void write_note(const char* text)
-{
-	FILE * history = fopen(HISTORY_PATH, "a");
-	
-	if (history == NULL)
-		return;
-	
-	time_t current_time;
-	time(&current_time);
-	struct tm * info = localtime(&current_time);
-	
-	char date_time[20];
-	strftime(date_time, sizeof(date_time), "%d-%m-%Y %H:%M:%S", info);
-	
-	fprintf(history, "[%s] \"%s\"\n", date_time, text);
-	
-	fclose(history);
-}
+void write_note(const char* text);
 
 #endif
