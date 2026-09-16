8da75c8243c4b4b126b14bc13cab25feacf66588
jean souza <741jean@gmail.com>
2026-09-16 00:45:41 -0300

    typewriter.h created for further implementation tomorrow

---
+++ j_tests/typewriter.h
@@ -0,0 +1,15 @@
+#ifndef TYPEWRITER_H
+#define TYPEWRITER_H
+
+#include <stdio.h>
+#include <time.h>
+
+/*
+this header file will contain a function that appends notes to a file (EXAMPLE: NOTES.md).
+1-the file path must be declared in a macro.
+2-every log or note will have it's time and date before it.
+3-every log or note will have its author.
+4-we will be using this as a way to show our progress during the project.
+*/
+
+#endif
